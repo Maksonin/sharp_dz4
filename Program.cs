3ea556e14aa381b8de64ad695c6bc2697d5f3824
Максим Трушкин <matrushkin@elavt.spb.ru>
2023-06-02 13:10:56 +0300

    Добавил решение остальных задач

--- Program.cs
+++ Program.cs
@@ -34,11 +34,61 @@ void Task25(){
 
 void Task27(){
     // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+    int SumOfNum(int num){
+        int result = 0;
+
+        while (num > 0){
+            result += num % 10;
+            num /= 10;
+        }
+
+        return result;
+    }
+
+    int num = Input("Введите число - ");
+    Console.WriteLine($"Сумма цифр числа {num} равна {SumOfNum(num)}");
 }
 
 void Task29(){
     //  Напишите программу, которая задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив.
 
+    void SetArray(int[] array){
+        Random rand = new Random();
+
+        for(int i = 0; i < array.Length; i++){
+            array[i] = rand.Next(-10,10);
+        }
+    }
+
+    void PrintArray(int[] array){
+        for(int i = 0; i < array.Length; i++)
+            Console.Write(array[i] + " ");
+            Console.WriteLine();
+    }
+
+    void SortArray(int[] array){
+        int tmp;
+
+        for(int i1 = 0; i1 < array.Length; i1++){
+            for(int i = 0; i < array.Length - 1; i++){
+                if(Math.Abs(array[i]) > Math.Abs(array[i+1])) {
+                    tmp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = tmp;
+                }
+            }
+        }
+
+    }
+
+    int[] array = new int[8];
+
+    SetArray(array);
+    PrintArray(array);
+    Console.WriteLine("------------------------------");
+    SortArray(array);
+    PrintArray(array);
+    Console.WriteLine("------------------------------");
 }
 
 void Menu() {
